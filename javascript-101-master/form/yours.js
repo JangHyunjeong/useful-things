cad2457967e153db9f5b077a879b80f5338e4849
Jang.hyunjeong <jang.hyunjeong01@gmail.com>
2021-07-11 17:04:21 +0900

    ✨ fake select

---
+++ javascript-101-master/form/yours.js
@@ -0,0 +1,26 @@
+const droopdown = document.querySelector('.dropdown');
+const toggleButton = document.querySelector('.dropdown-toggle');
+const menu = document.querySelector('.dropdown-menu');
+const options = document.querySelectorAll('.dropdown-option');
+const nextButton = document.querySelector('.next-button');
+
+
+toggleButton.addEventListener('click',function(){
+    menu.classList.toggle('show');
+});
+toggleButton.addEventListener('blur',function(){
+    menu.classList.remove('show');
+});
+
+options.forEach(function(item){
+    item.addEventListener('click',function(e){
+       const value = e.currentTarget.textContent.trim();
+       //console.log(value); 
+
+        toggleButton.textContent = value;
+        toggleButton.classList.add('selected');
+        nextButton.removeAttribute('disabled');
+
+    });
+});
+
